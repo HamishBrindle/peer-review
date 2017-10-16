de76a50ca5f67aa81999563fb80384742208cd78
HamishBrindle <hamishbrindle@gmail.com>
2017-10-16 12:50:47 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+session_start();
+
+include('includes/head.html');
+
+?>
+
+<div class="site-wrapper">
+
+     <div class="site-wrapper-inner">
+
+         <div class="cover-container">
+
+             <div class="masthead clearfix">
+                 <div class="inner">
+                     <h3 class="masthead-brand">Peer Review</h3>
+                     <nav class="nav nav-masthead">
+                         <a class="nav-link active" href="#">Home</a>
+                         <a class="nav-link" href="#">Features</a>
+                         <a class="nav-link" href="#">Contact</a>
+                     </nav>
+                 </div>
+             </div>
+
+             <div class="inner cover">
+                 <h1 class="cover-heading">Make sure your code isn't shit!</h1>
+                 <p class="lead">Post your code and have it reviewed by the finest minds in the BCIT CST program. We'll tell you what can stay, needs fixing, and needs to go!</p>
+                 <p class="lead">
+                     <a href="#" class="btn btn-lg btn-secondary">Start Now</a>
+                 </p>
+             </div>
+
+             <div class="mastfoot">
+                 <div class="inner">
+                     <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+                 </div>
+             </div>
+
+         </div>
+
+     </div>
+
+ </div>
+ <?php<?=
+
+include('includes/foot.html');
+
+  ?>
