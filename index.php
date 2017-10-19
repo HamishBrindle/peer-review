6b8703c2b12643ee0614a977c27457ce9321e604
HamishBrindle <hamishbrindle@gmail.com>
2017-10-18 21:48:32 -0700

    Removed crap

--- index.php
+++ index.php
@@ -8,38 +8,19 @@ include('includes/head.html');
 
          <div class="cover-container">
 
-             <div class="masthead clearfix">
-                 <div class="inner">
-                     <h3 class="masthead-brand">Peer Review</h3>
-                     <nav class="nav nav-masthead">
-                         <a class="nav-link active" href="#">Home</a>
-                         <a class="nav-link" href="#">Features</a>
-                         <a class="nav-link" href="#">Contact</a>
-                     </nav>
-                 </div>
-             </div>
+             <?php
+             include_once('includes/navigation.html');
+             ?>
 
              <div class="inner cover">
                  <h1 class="cover-heading">Make sure your code isn't shit!</h1>
                  <p class="lead">Post your code and have it reviewed by the finest minds in the BCIT CST program. We'll tell you what can stay, needs fixing, and needs to go!</p>
                  <p class="lead">
-                     <a href="start.php" class="btn btn-lg btn-secondary">Start Now</a>
+                     <a href="submit.php" class="btn btn-lg btn-secondary">Start Now</a>
                  </p>
              </div>
 
-             <div class="mastfoot">
-                 <div class="inner">
-                     <p>A sweet site by <a href="https://github.com/hamishbrindle">Hamish</a> and <a href="https://github.com/sheldonlynn">Sheldon</a>
-                 </div>
-             </div>
-         </div>
 
-<<<<<<< HEAD
-=======
-     </div>
-
- </div>
->>>>>>> 883d37545a847dd49391ec5bd99d516256ae7134
  <?php
 
 include('includes/foot.html');
--- submit.php
+++ submit.php
@@ -6,45 +6,20 @@ include('includes/head.html');
 
 ?>
 
-<div class="site-wrapper">
+    <div class="cover-container">
 
-    <div class="site-wrapper-inner">
+        <?php
+        include_once('includes/navigation.html');
+        ?>
 
-        <div class="cover-container">
-
-            <div class="masthead clearfix">
-                <div class="inner">
-                    <h3 class="masthead-brand">Peer Review</h3>
-                    <nav class="nav nav-masthead">
-                        <a class="nav-link active" href="#">Home</a>
-                        <a class="nav-link" href="#">Features</a>
-                        <a class="nav-link" href="#">Contact</a>
-                        <a class="nav-link" href="submit.php">Submit Code</a>
-                    </nav>
-                </div>
-            </div>
-
-            <div class="inner cover">
-                <h1 class="cover-heading">Peer Review Your Code</h1>
-                <form name="submit-code" action="">
-                    <textarea id="submit-code"></textarea>
-                    <br>
-                    <input type="submit" class="lead btn btn-lg btn-secondary"/>
-                </form>
-            </div>
-
-            <div class="mastfoot">
-                <div class="inner">
-                    <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-                </div>
-            </div>
-
+        <div class="inner cover">
+            <h1 class="cover-heading">Peer Review Your Code</h1>
+            <form name="submit-code" action="" style="text-align: left;">
+                <textarea id="submit-code"></textarea>
+                <br>
+                <input type="submit" class="lead btn btn-lg btn-secondary"/>
+            </form>
         </div>
-
-    </div>
-
-</div>
-
 
 <script
         src="https://code.jquery.com/jquery-3.2.1.min.js"
