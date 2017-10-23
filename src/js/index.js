fd1412ea10e776cb1bf96f88cbad6442cb48bce7
HamishBrindle <hamishbrindle@gmail.com>
2017-10-22 17:36:02 -0700

    Removed redundant webpack script for scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,6 @@
 import _ from 'lodash';
 import '../scss/main.scss';
 
-function component() {
-    var element = document.createElement('div');
-
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
-
 /* Building the modal
  ----------------------------------------------------------------------------*/
 var modal = document.getElementById('myModal');
