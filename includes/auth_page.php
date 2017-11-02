616cd722a2c3ac35d972fc9873ac6e51f7e00ed1
HamishBrindle <hamishbrindle@gmail.com>
2017-11-01 17:15:58 -0700

    Added environment variables to allow for easy access

--- includes/auth_page.php
+++ includes/auth_page.php
@@ -4,20 +4,14 @@ require("vendor/autoload.php");
 require("includes/phpauth/phpauth/Config.php");
 require("includes/phpauth/phpauth/Auth.php");
 
-$dbh = new PDO("mysql:host=localhost;dbname=roast", "root", "1234");
+$dotenv = new Dotenv\Dotenv(getcwd());
+$dotenv->load();
+
+// $dbh = new PDO("mysql:host=" . getenv('MYSQL_HOST') . ";dbname=" . getenv('DB_DATABASE') . ", " . getenv('DB_USERNAME') . ", " . getenv('DB_PASSWORD'));
+$dbh = new PDO('mysql:host=' . getenv('MYSQL_HOST') . ';dbname=' . getenv('DB_DATABASE'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
 
 $config = new PHPAuth\Config($dbh);
 $auth   = new PHPAuth\Auth($dbh, $config);
-//$register = $auth->register('shel@sheldon.com', 'sheldon1234!', 'sheldon1234!');
-
-$hash = $auth->login('sheld@sheldon.com', 'sheldon1234!');
-
-if (isset($hash['hash'])) {
-    setcookie('authID', $hash['hash'], time() + 9000000000);
-} else {
-    header('HTTP/1.0 403 Forbidden');
-    echo($hash['error'][1]);
-}
 
 if (!$auth->isLogged()) {
 header('HTTP/1.0 403 Forbidden');
